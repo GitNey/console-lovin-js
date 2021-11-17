e8b3141aec51f73a46be52f397a4e0217bff0ad2
nher1625 <nher1625@gmail.com>
2021-11-16 22:40:00 -0500

    Carouselfy it all...

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function main () {
 
   if (checkDeps()) {
     window.$('#demo-1').on('click', demo1)
+    window.$('#nav-start-btn').on('click', startSlideShow)
   }
 }
 
@@ -15,6 +16,7 @@ function checkDeps() {
   try {
     if (window.$) {
       window.console.info('jQuery 3 dependency is present...')
+      window.console.groupEnd('Dependency check')
       return true
     } else {
       throw Error('Project missing dependency: jQuery')
@@ -28,4 +30,12 @@ function checkDeps() {
 
 const demo1 = function(ev) {
   window.console.log('hey there cutie')
+}
+
+const startSlideShow = function (ev) {
+  try {
+    window.$('#main-carousel').carousel(1)  
+  } catch(err) {
+    window.console.error('Error navigating to carousel slide:', err)
+  }
 }
