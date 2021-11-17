771fcfbc87c6a5cc3186de19329c401c6ce97a71
nher1625 <nher1625@gmail.com>
2021-11-16 23:52:10 -0500

    Add console.assert

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function main () {
   window.console.info('Running index.js!')
 
   if (checkDeps()) {
-    window.$('#demo-1').on('click', demo1)
+    window.$('button#demo-1').on('click', demo1)
     window.$('#nav-start-btn').on('click', startSlideShow)
+    window.$('button#demo-2').on('click', demo2)
+    window.$('#go-to-slide').on('click', goToSection)
   }
 }
 
@@ -22,7 +24,7 @@ function checkDeps() {
       throw Error('Project missing dependency: jQuery')
     }
   } catch (err) {
-    window.console.log('Dependency check error:', err)
+    window.console.error('Dependency check error:', err)
   }
   window.console.groupEnd('Dependency check')
   return false
@@ -38,4 +40,13 @@ const startSlideShow = function (ev) {
   } catch(err) {
     window.console.error('Error navigating to carousel slide:', err)
   }
+}
+
+const demo2 = function (ev) {
+  window.console.assert(1 > 2, 'Error: 1 is less than 2!')
+}
+
+const goToSection = function (ev) {
+  const slideIndex = parseInt(window.$(ev.target).attr('data-slide'))
+  window.$('#main-carousel').carousel(slideIndex)
 }
